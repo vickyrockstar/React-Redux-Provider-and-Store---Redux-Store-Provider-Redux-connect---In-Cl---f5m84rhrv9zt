b90f4633e755a39725760fd8f7c0ea3f898c6f12
vighnesh shetty <116479689+vickyrockstar@users.noreply.github.com>
2023-04-10 19:44:46 +0530

    second

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 //code here 
 
-import {Provider} from "react-redux";
-import { creatStore} from "redux";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import reducers from "./reducers/index.js";
 
 
 
 ReactDOM.render(
-<Provider store={creatStore(reducers)}>
-<App />
+<Provider store={createStore(reducers)}>
+    <App />
 </Provider>, 
 document.getElementById("root")
 );
